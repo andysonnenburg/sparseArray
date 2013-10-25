b64348979341851142bee2a1c83dc4c4682c49ef
Andy Sonnenburg <andy22286@gmail.com>
2013-10-25 16:48:02 -0500

    whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,25 @@
 module.exports = function(grunt) {
 	'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
 			files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-      options: {
-        curly: true,
-        eqeqeq: true,
+			options: {
+				curly: true,
+				eqeqeq: true,
 				es3: true,
 				forin: true,
-        immed: true,
-        latedef: 'nofunc',
-        newcap: true,
-        noarg: true,
+				immed: true,
+				latedef: 'nofunc',
+				newcap: true,
+				noarg: true,
 				noempty: true,
 				nonew: true,
 				plusplus: true,
 				quotmark: 'single',
-        undef: true,
+				undef: true,
 				unused: true,
 				strict: true,
 				trailing: true,
@@ -27,18 +27,18 @@ module.exports = function(grunt) {
 					module: false,
 					define: false
 				}
-      }
-    },
-    watch: {
+			}
+		},
+		watch: {
 			scripts: {
 				files: '<%= jshint.files %>',
 				tasks: ['jshint']
 			}
-    }
-  });
+		}
+	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['watch']);
+	grunt.registerTask('default', ['watch']);
 };
--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-sparseArray
-===========
+# sparseArray
 
-Sparse Arrays for JavaScript
+Sparse Array
+
+## Getting Started
+Install the module with: `npm install sparseArray`
+
+```javascript
+var sparseArray = require('sparseArray');
+sparseArray.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/gruntjs/grunt).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Andy Sonnenburg <andy22286@gmail.com>  
+Licensed under the BSD license.
